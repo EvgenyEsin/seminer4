418a6a8ebaf6a3ad3a57269787c9fb39302a65bb
Evgeniy <zigzagmed@yandex.ru>
2023-01-26 23:25:43 +0300

    ДЗ к семинару №4

---
+++ task25/Program.cs
@@ -0,0 +1,48 @@
+﻿// 25. Напишите цикл, который принимает на вход два числа (А и В)
+// и возвращает число А в натуральную степень В
+// 3, 5 -> 243 (3 в 5 )
+// 2, 4 -> 16
+
+// Вариент 1 с использованием цикла for
+int Stepen (int numberA, int numberB)
+{
+    int result = 1;
+    if (numberB == 0) return result;
+    else 
+    {
+        for (int i = 1; i <= numberB; i++)
+            result = result * numberA;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("В какую степень его возвести?: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{numberA} в степени {numberB} равно {Stepen(numberA, numberB)}");
+
+
+// Вариант 2 с использованием цикла while
+int Stepen2 (int numA, int numB)
+{
+    int res = 1;
+    if (numB == 0) return res;
+    else 
+    {
+        int count = 1;
+        while (count <= numB)
+        {
+            res = res * numA;
+            count ++;
+        }
+            
+    }
+    return res;
+}
+
+Console.WriteLine("Введите число А: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("В какую степень его возвести?: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{numA} в степени {numB} равно {Stepen2(numA, numB)}");
---
+++ task29/Program.cs
@@ -0,0 +1,34 @@
+﻿// 29 Напишите программу, которая задает массив из 8 элементов
+// и выводит их на экран
+
+string CreateRandomArray ()
+{
+    int [] array = new int [8];
+    for (int i = 0; i < 8; i++)
+    {
+        array[i] = new Random().Next(0, 20);
+        Console.Write(array[i] + ", ");
+    }
+    return "\b\b ";
+}
+Console.Write(CreateRandomArray ());
+
+// Вариант2* Напишите программу, которая задает массив из N элементов
+// в заданном пользователем диапазоне и выводит их на экран
+
+string CreateRandomArray2 (int numElement, int diap)
+{
+    int [] array2 = new int [numElement];
+    for (int i = 0; i < numElement; i++)
+    {
+        array2[i] = new Random().Next(0, (diap + 1));
+        Console.Write(array2[i] + ", ");
+    }
+    return "\b\b ";
+}
+
+Console.WriteLine("Сколько элементов в массиве?: ");
+int numElement = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Максимальное значение элемента (не больше 20)?: ");
+int diap = Convert.ToInt32(Console.ReadLine());
+Console.Write(CreateRandomArray2(numElement, diap));
